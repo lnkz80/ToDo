2bb21b108a476515c03bbd7b0c1cbd10cf7370f4
lnkz <lnkz80@gmail.com>
2022-10-23 23:26:07 +0300

    Render data from server - Done

--- js/script.js
+++ js/script.js
@@ -5,9 +5,8 @@ document.addEventListener("DOMContentLoaded", () => {
     todosUrl = "http://localhost:3000/todos",
     usersSelector = ".selectUser select",
     todoListSelector = ".todoContent";
-
-  //*ASYNC...AWAIT
-// console.log(first)
+    
+  //? Додати перевірку на помилки при з'єднанні
   const getData = async (url) => {
     const res = await fetch(url);
     if (!res.ok) {
@@ -16,72 +15,48 @@ document.addEventListener("DOMContentLoaded", () => {
     return await res.json();
   };
 
-  // Get Data from server
+  // Render Data from server
   let users = [],    
-  todos = [];
+  todos = []; 
 
-  getData(usersUrl).then((data) => {
-    data.forEach((item) => users.push(item));
-  });
-
-  getData(todosUrl).then((data) => {
-  });
-  const renderUsers = () => {
-    // console.log(users);
-    //! users видає пусту строку при переборі:
-    users.forEach((item) => console.log(item.name));
+  const renderUsers = async () => {
+    users = await getData(usersUrl);
+    users.forEach(item=>{
+      document.querySelector(usersSelector).innerHTML += `
+      <option value=${item.id}>${item.name} (${item.position})</option>
+      `;
+    });
   };
-
+  
+  const renderTodos = async () => {
+    todos = await getData(todosUrl);
+    const objDomPlace = document.querySelector(todoListSelector);   
+    if (todos.length !== 0) {
+      todos.forEach(item => {
+        objDomPlace.innerHTML += `
+        <li>
+        <i class="fa-regular ${item.status?'fa-square-check todo-done':'fa-square'}"></i>
+        ${item.user}: ${item.todo}
+        <i class="fa-solid fa-xmark todo-close"></i>
+        </li>`;
+      });
+      addListenerToTodos(objDomPlace.children);
+    } else {
+      objDomPlace.innerHTML = "<li><h4>There is no todos yet...</h4></li>";
+    }
+  };
+  
+  //AddEventListeners
+  const addListenerToTodos = (todoCollection) => {    
+    //!Додати функціонал додавання в БД виконанних завдань та їх видалення
+    for(let item of todoCollection) {
+      item.addEventListener("click", function (e) {
+        this.firstElementChild.classList.remove("fa-square");
+        this.firstElementChild.classList.add("fa-square-check", "todo-done");
+      });
+    }
+  };
+  
   renderUsers();
-
-  //   console.log(users);
-  //   console.log(todos);
-
-  //*=======> FETCH().THEN()
-  //   const renderData = (typeOfData, url, objSelector) => {
-  //     const objDomPlace = document.querySelector(objSelector);
-  //     //!зробити спіннер підгрузки тудушок
-  //     fetch(url)
-  //       .then((res) => res.json())
-  //       .then((data) => {
-  //         objDomPlace.innerHTML =
-  //           typeOfData === "todos" && data.length !== 0
-  //             ? ""
-  //             : "<li><h4>There is no todos yet...</h4></li>";
-
-  //         data.forEach((item) => {
-  //           if (typeOfData === "users") {
-  //             objDomPlace.innerHTML += `<option value=${item.id}>${item.name} (${item.position})</option>`;
-  //           }
-  //           if (typeOfData === "todos") {
-  //             objDomPlace.innerHTML += `
-  //             <li>
-  //               <i class="fa-regular fa-square unchecked"></i>
-  //               ${item.user}: ${item.todo}
-  //               <i class="fa-solid fa-xmark close"></i>
-  //             </li>`;
-  //           }
-
-  //         });
-  //       })
-  //       .catch((err) => console.error("Виникла помилка: ", err));
-  //   };
-
-  //   renderData("users", usersUrl, usersSelector);
-  //   renderData("todos", todosUrl, todoListSelector);
-
-  //   console.log(document.querySelector(todoListSelector).children);
-  //? зробити окрему функцію зміни значка, та зробити перевірку на чек/анчек
-
-  //   for (const item of document.querySelectorAll(`.todoContent li`)) {
-  //     console.log(item);
-  //   }
-
-  //   [...document.querySelector(todoListSelector).children].forEach((item) => {
-  //     item.addEventListener("click", function (e) {
-  //       //!Перевірити наявність виконаних вже завдань завантажених з БД
-  //       this.firstElementChild.classList.remove("fa-square", "unchecked");
-  //       this.firstElementChild.classList.add("fa-square-check", "checkedDone");
-  //     });
-  //   });
+  renderTodos();  
 });
